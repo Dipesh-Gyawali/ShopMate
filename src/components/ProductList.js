1a571bb61ab879675a4193e2071257398235671f
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-03-21 18:12:06 +0545

    first commit

---
+++ src/components/ProductList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+export const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect (() => {
+        fetch("http://localhost:8000/products")
+        .then(response => response.json())
+        .then(data => setProducts(data));
+    },[]);
+
+  return (
+    <section>
+        <h1>Data Fetching using useEffect</h1>
+
+        
+        {products.map((product)=> (
+            <div className='card' key={product.id}>
+                <p className='id'>{product.id}</p>
+                <p className='name'>{product.name}</p>
+                <p className='info'>
+                    <span>${product.price}</span>
+                    <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
+                </p>
+            </div> 
+            
+        )) }  
+    </section>          
+
+  )
+}
