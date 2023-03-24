7bab7d1bc33ffbf1fd0e8fd2e831f28c5c5dcd26
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-03-24 19:43:55 +0545

    Aborting Fetch request

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react"
 export const useFetch = (url) => {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("")
-
+    const [error, setError] = useState("");
+    
     useEffect(() => {
+        const controller = new AbortController();
+        
         const fetchData = async () => {
             setLoading(true);
             try{
-                const response = await fetch(url);
+                const response = await fetch(url, {signal:controller.signal});
                 if(!response.ok){
                     throw new Error(response.statusText);
                 }    
@@ -22,7 +24,10 @@ export const useFetch = (url) => {
                 setError(error.message);
             }    
         }
-        fetchData();
+        fetchData(); 
+
+        return () => controller.abort();
+        
     }, [url])
     
   return { data, loading, error }
