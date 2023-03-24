745fe52870d4eb9a85c2252ba5ad863e3563e611
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-03-24 19:38:33 +0545

    handling error

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,7 @@ import { useFetch } from '../hooks/useFetch';
 
 export const ProductList = () => {
     const [url, setUrl] = useState(["http://localhost:8000/products"]);
-    const { data : products, loading } = useFetch(url);
+    const { data : products, loading, error } = useFetch(url);
 
   return (
     <section>
@@ -16,6 +16,8 @@ export const ProductList = () => {
         </div>
 
         { loading && <p>Loaddddddddding Products</p> }
+
+        { error && <p> { error } </p>}
 
         {products && products.map((product)=> (
             <div className='card' key={product.id}>
